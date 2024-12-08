6894aa1ef50d853e679ad074954735fb6ada5f22
Tung <hztung3004@gmail.com>
2024-12-08 21:26:08 +0700

    #17 - Tung - Fix PrescriptionResource

--- app/Http/Resources/PrescriptionResource.php
+++ app/Http/Resources/PrescriptionResource.php
@@ -18,9 +18,28 @@ class PrescriptionResource extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'description' => $this->description,
-            'user_id' => $this->user_id,
-            'patient_id' => $this->patient_id,
-            'medical_histories_id' => $this->medical_histories_id,
+            'user' => $this->user ? [
+                'id' => $this->user->id,
+                'status' => $this->user->status,
+                'role' => $this->user->role ? [
+                    'id' => $this->user->role->id,
+                    'name' => $this->user->role->name,
+                    'description' => $this->user->role->description,
+                ] : null,
+                'user_info' => $this->user->userInfo,
+            ] : null,
+            'patient' => $this->patient ? [
+                'id' => $this->patient->id,
+                'insurance_number' => $this->patient->insurance_number,
+                'identity_card_id' => $this->patient->identity_card_id,
+                'status' => $this->patient->status,
+            ] : null,
+            'medical_histories' => $this->medical_histories ? [
+                'id' => $this->id,
+                'description' => $this->description,
+                'diagnosis' => $this->diagnosis,
+                'treatment' => $this->treatment
+            ] : null,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
             'medications' => PrescriptionInfoResource::collection($this->prescription_Infos)
